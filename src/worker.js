516b946612a0dc51de41da3d07b53bba6b25eaad
rtldg <rtldg@protonmail.com>
2024-03-25 01:01:30 +0000

    poke 3

--- src/worker.js
+++ src/worker.js
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: WTFPL
 
 export default {
-    async fetch(request) {
-      const url = new URL(request.url);
-      if (!url.pathname.startsWith("/hc/") || !url.pathname.endsWith(".jpg"))
-        return new Response("");
-      return await fetch(`https://hc.fly.dev${url.pathname}?${(new Date()).getUTCDate()}${crypto.randomUUID()}`, {cf:{cacheTtl:0}});
-    },
-  };
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (!url.pathname.startsWith("/hc/") || !url.pathname.endsWith(".jpg"))
+      return new Response("");
+    return await fetch(`https://hc.fly.dev${url.pathname}?${(new Date()).getUTCDate()}${crypto.randomUUID()}`, {cf:{cacheTtl:0}});
+  },
+};
